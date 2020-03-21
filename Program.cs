354e81f4f89b60ddfda69c507f06fcc634b4e62e
ProshNad <n.proshutina@gmail.com>
2020-03-21 14:54:12 +0300

    lab6

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 namespace lab7
 {
     class Program
@@ -29,6 +31,32 @@ namespace lab7
 
             Console.WriteLine("Лаба 6:");
             Console.WriteLine("Введите произвольное количество слов:");
+            string inp2 = Console.ReadLine();
+            string[] w = inp2.Split(' ');
+            List<string> li = new List<string>(w);
+
+
+            string temp2="";
+            for (int i = 0; i < li.Count; i++)
+            {
+                for (int j = i + 1; j < li.Count; j++)
+                {
+                    if (li[i][0] > li[j][0])
+                    {
+                        temp2 = li[i];
+                        li[i] = li[j];
+                        li[j] = temp2;
+                    }
+                }
+            }
+            var r2= String.Join(" ", li.ToArray());
+            Console.WriteLine($"Отсортировали по алфавиту: {r2}");
+
+            string str = "";
+            for (int i = 0; i < li.Count; i++) {
+                str = str + li[i][li[i].Length-1];
+            }
+            Console.WriteLine($"Получили слово: {str}");
 
 
 
